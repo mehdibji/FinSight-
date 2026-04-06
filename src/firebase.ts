a02038f06ba44f2d450dd19d04dc9e7cf24a96c7
mehdibji <mehdibouji07@gmail.com>
2026-04-06 14:19:41 +0200

    Refactor Firebase initialization and configuration
    Removed unused imports and test connection function. Added Firebase configuration directly in the file.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,27 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User } from 'firebase/auth';
-import { getFirestore, collection, addDoc, query, where, onSnapshot, orderBy, limit, doc, getDocFromServer } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// Import the Firebase configuration
-import firebaseConfig from '../firebase-applet-config.json';
+const firebaseConfig = {
+ apiKey: "AIzaSyBW8pVk_omIHrH95PqDPlnIZsI9tjkGFUI",
+  authDomain: "finsight-d27a6.firebaseapp.com",
+  projectId: "finsight-d27a6",
+  storageBucket: "finsight-d27a6.firebasestorage.app",
+  messagingSenderId: "494049581436",
+  appId: "1:494049581436:web:cacb4ccd22e51e0e9eacf9",
+  measurementId: "G-XTGP2G6M0F"
+};
 
-// Initialize Firebase SDK
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-
-export { signInWithPopup, signOut, onAuthStateChanged };
-export type { User };
-
-// Test connection
-async function testConnection() {
-  try {
-    await getDocFromServer(doc(db, 'test', 'connection'));
-  } catch (error) {
-    if(error instanceof Error && error.message.includes('the client is offline')) {
-      console.error("Please check your Firebase configuration. ");
-    }
-  }
-}
-testConnection();
